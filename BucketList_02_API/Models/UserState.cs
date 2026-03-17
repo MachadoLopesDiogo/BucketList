ae62f882b59c81c14ddd0ef1e146c0976610a9c3
Diogo Machado Lopes <lopesdiogo465@gmail.com>
2026-03-17 13:04:09 +0100

    nav menu buckelisitem

--- BucketList_02_API/Models/UserState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BucketList_02_API.Models
-{
-    public class UserState
-    {
-        public int UserId { get; set; }
-        public string UserName { get; set; } = "";
-        public bool IsLoggedIn => UserId > 0;
-    }
-}
